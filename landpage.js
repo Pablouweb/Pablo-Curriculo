83a6b507f346360e9fe2a954d63c5b278beacfeb
Pablouweb <110791273+Pablouweb@users.noreply.github.com>
2024-11-11 22:14:59 -0300

    Add files via upload

---
+++ landpage.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal(".nomeproprio ", { duration: 1000 });
+sr.reveal(".scrollreveal ", { duration: 2000 });
+sr.reveal(".scrollreveal. ", { duration: 3000 });
